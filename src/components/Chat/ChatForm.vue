864df50e63fe50688d4eb0ca49748dded420d8dd
Wesley Roekens <Roekenswesley@gmail.com>
2024-10-24 15:12:13 +0200

    Challenge4_problem_api

--- src/components/Chat/ChatForm.vue
+++ src/components/Chat/ChatForm.vue
@@ -1,26 +1,13 @@
 <script setup>
   import { ref, reactive } from 'vue';
 
-  // let name = ref("Anomi");
-  // let comments = reactive([
-  //   {name: "Anomi",message: "Hello, how are you?"},
-  //   {name: "Omi",message: "I'm fine, thank you!"},
-  //   {name: "Sheba",message: "WEEEEEEEEEEEE!"}
-  // ]);
 
   function doit(){
-    // comments.push({name: name.value, message: "Hello"});
   }
 </script>
 
 <template>
   <div class="form">
-    <!-- <h3>{{ name }}</h3>
-    <ul>
-      <li v-for="comment in comments">
-        <strong>{{ comment.name }}</strong>: {{ comment.message }}
-      </li>
-    </ul> -->
 
     <input  type="text" placeholder="Type your message here" />
     <button @click="doit">Send</button>
